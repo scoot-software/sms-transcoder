645f3ee89f3a1258a60fc194d7bf66bf8af12b7a
Scott Ware <scooter1556@msn.com>
2019-04-30 21:14:09 +0100

    Update FFmpeg and x265 libraries
    Signed-off-by: Scott Ware <scooter1556@msn.com>

--- build.sh
+++ build.sh
@@ -30,8 +30,8 @@ download () {
     tar -xf "$DOWNLOAD_DIR"/last_x264.tar.bz2 -C "$BUILD_DIR"
 
     echo "*** Downloading x265 ***"
-    wget -N https://bitbucket.org/multicoreware/x265/downloads/x265_2.9.tar.gz -P "$DOWNLOAD_DIR"
-    tar -xf "$DOWNLOAD_DIR"/x265_2.9.tar.gz -C "$BUILD_DIR"
+    wget -N https://bitbucket.org/multicoreware/x265/downloads/x265_3.0.tar.gz -P "$DOWNLOAD_DIR"
+    tar -xf "$DOWNLOAD_DIR"/x265_3.0.tar.gz -C "$BUILD_DIR"
 
     echo "*** Downloading Fraunhofer FDK AAC ***"
     wget -N https://github.com/mstorsjo/fdk-aac/archive/v0.1.6.tar.gz -O "$DOWNLOAD_DIR"/fdk-aac-v0.1.6.tar.gz
@@ -40,11 +40,11 @@ download () {
     echo "*** Downloading LAME MP3 ***"
     wget -N https://datapacket.dl.sourceforge.net/project/lame/lame/3.100/lame-3.100.tar.gz -P "$DOWNLOAD_DIR"
     tar -xf "$DOWNLOAD_DIR"/lame-3.100.tar.gz -C "$BUILD_DIR"
-    
+
     echo "*** Downloading Ogg ***"
     wget -N http://downloads.xiph.org/releases/ogg/libogg-1.3.3.tar.gz -P "$DOWNLOAD_DIR"
     tar -xf "$DOWNLOAD_DIR"/libogg-1.3.3.tar.gz -C "$BUILD_DIR"
-    
+
     echo "*** Downloading Vorbis ***"
     wget -N http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.6.tar.gz -P "$DOWNLOAD_DIR"
     tar -xf "$DOWNLOAD_DIR"/libvorbis-1.3.6.tar.gz -C "$BUILD_DIR"
@@ -54,8 +54,8 @@ download () {
     tar -xf "$DOWNLOAD_DIR"/nv-codec-headers-8.1.24.2.tar.gz -C "$BUILD_DIR"
 
     echo "*** Downloading FFmpeg ***"
-    wget -N https://github.com/FFmpeg/FFmpeg/archive/19546cfef103bdc967b0e06ec35c96766e3febec.zip -O "$DOWNLOAD_DIR"/ffmpeg.zip
-    unzip "$DOWNLOAD_DIR"/ffmpeg.zip -d "$BUILD_DIR"
+    wget -N https://github.com/FFmpeg/FFmpeg/archive/n4.1.3.tar.gz -O "$DOWNLOAD_DIR"/ffmpeg.tar.gz
+    tar -xf "$DOWNLOAD_DIR"/ffmpeg.tar.gz -C "$BUILD_DIR"
 }
 
 # Build
@@ -101,13 +101,13 @@ build () {
     ./configure --prefix=$TARGET_DIR --enable-nasm --disable-shared
     make
     make install
-    
+
     echo "*** Building Ogg ***"
     cd $BUILD_DIR/libogg*
     ./configure --prefix=$TARGET_DIR --disable-shared
     make
     make install
-    
+
     echo "*** Building Vorbis ***"
     cd $BUILD_DIR/libvorbis*
     ./configure --prefix=$TARGET_DIR --with-ogg=$TARGET_DIR --disable-shared
@@ -126,7 +126,7 @@ build () {
     --pkg-config-flags="--static" \
     --extra-cflags="-I$TARGET_DIR/include" \
     --extra-ldflags="-L$TARGET_DIR/lib" \
-    --extra-libs="-lpthread -lm -lz" \
+    --extra-libs="-lpthread" \
     --enable-static \
     --disable-debug \
     --enable-pic \
